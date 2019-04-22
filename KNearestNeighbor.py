f47170307f5a0216a69c92009293baa6dab9c0cc
Andy <andrewwerner1@gmail.com>
2019-04-22 16:42:19 -0400

    updates

--- KNearestNeighbor.py
+++ KNearestNeighbor.py
@@ -2,13 +2,13 @@ import numpy as np
 import operator
 import GetData as dataHandler
 
-def find_k_nearest_neighbors(test_example, training_data, k):
+def find_k_nearest_neighbors(x_train, test_point, k):
     distances = []
     neighbors = []
-    number_of_training_examples = training_data.shape[0]
+    number_of_training_examples = x_train.shape[0]
     for i in range(0, number_of_training_examples):
-        training_example = training_data[i]
-        dist = find_distance(training_example, test_example)
+        training_example = x_train[i]
+        dist = find_distance(training_example, test_point)
         distances.append((i, dist))
     distances.sort(key=operator.itemgetter(1))
     for x in range(k):
@@ -22,11 +22,31 @@ def find_distance(vector1, vector2):
     return distance
 
 
-def K_Nearest_Neighbor(x_train, x_test, y_train, y_test, k):
+
+def predictKnnClassForRegression(neighbor_indices, y_train):
+    pass
+
+def predictKnnClassForClassification(neighbor_indices, y_train):
+    classVotes = {}
+    for i in range(0, len(neighbor_indices)):
+        neighbor_index = neighbor_indices[i]
+        neighbor = y_train[neighbor_index]
+        if neighbor in classVotes:
+            classVotes[neighbor] += 1
+        else:
+            classVotes[neighbor] = 1
+    sortedVotes = sorted(classVotes.items(), key=operator.itemgetter(1), reverse=True)
+    return sortedVotes[0][0]
+
+
+def K_Nearest_Neighbor(x_train, x_test, y_train, y_test, k, classification=True):
     output_classes = []
     for i in range(0, x_test.shape[0]):
-        neighbors = find_k_nearest_neighbors(x_train, x_test[i], k)
-        predictedClass = predictKnnClass(neighbors, y_train)
+        neighbor_indices = find_k_nearest_neighbors(x_train, x_test[i], k)
+        if(classification):
+            predictedClass = predictKnnClassForClassification(neighbor_indices, y_train)
+        else:
+            predictedClass = predictKnnClassForRegression(neighbor_indices, y_train)
         output_classes.append((predictedClass))
     return output_classes
 
