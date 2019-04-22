b4238c871a2f45f76b2947466d2a89184d218faf
Andy <andrewwerner1@gmail.com>
2019-04-22 15:23:01 -0400

    updating

--- KNearestNeighbor.py
+++ KNearestNeighbor.py
@@ -1,7 +1,6 @@
-# -*- coding: utf-8 -*-
 import numpy as np
-import copy
-
+import operator
+import GetData as dataHandler
 
 def find_k_nearest_neighbors(test_example, training_data, k):
     distances = []
@@ -13,15 +12,34 @@ def find_k_nearest_neighbors(test_example, training_data, k):
         distances.append((i, dist))
     distances.sort(key=operator.itemgetter(1))
     for x in range(k):
-        neighbors.append()
+        neighbors.append(distances[x][0])
+    return neighbors
 
 
 def find_distance(vector1, vector2):
     difference = vector1 - vector2
     distance = (np.dot(difference ,difference)) ** .5
-    return distance;
+    return distance
 
 
-def K_Nearest_Neighbor(training_data, test_data, k):
-    for vector in test_data:
-        K_nearest_neihbors = find_k_nearest_neighbors(vector, training_data, k);
+def K_Nearest_Neighbor(x_train, x_test, y_train, y_test, k):
+    output_classes = []
+    for i in range(0, x_test.shape[0]):
+        neighbors = find_k_nearest_neighbors(x_train, x_test[i], k)
+        predictedClass = predictKnnClass(neighbors, y_train)
+        output_classes.append((predictedClass))
+    return output_classes
+
+#test
+print('test')
+dataset = dataHandler.get_data_from_file("C:/Users/WernerAS/PycharmProjects/TestEnv/Data/IrisModified.csv")
+split_data_sets = dataHandler.get_k_folds(dataset, k=5)
+
+test_set = split_data_sets[0]
+x_test, y_test = dataHandler.split_data_into_XY(test_set, 4, 0, 3)
+training_set = dataHandler.concatenate_sets(split_data_sets, 1, 5)
+x_train, y_train = dataHandler.split_data_into_XY(training_set, 4, 0, 3)
+
+
+k = 4
+K_Nearest_Neighbor(x_train, x_test, y_train, y_test, k)
