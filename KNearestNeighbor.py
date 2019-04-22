c3c1b6cf371b014e7f0e4987a4deb06f647b436f
Andy <andrewwerner1@gmail.com>
2019-04-21 20:17:57 -0400

    small updates

--- KNearestNeighbor.py
+++ KNearestNeighbor.py
@@ -1,4 +1,20 @@
 # -*- coding: utf-8 -*-
 import numpy as np
+import copy
+
+
+def find_k_nearest_neighbors(vector, training_data, k):
+    training_data_copy = copy.deepcopy(training_data)
+    for i in range(1, k):
+        pass
+
+
+def find_distance(vector1, vector2):
+    difference = vector1 - vector2
+    distance = (np.dot(difference ,difference)) ** .5
+    return distance;
+
 
 def K_Nearest_Neighbor(training_data, test_data, k):
+    for vector in test_data:
+        K_nearest_neihbors = find_k_nearest_neighbors(vector, training_data, k);
